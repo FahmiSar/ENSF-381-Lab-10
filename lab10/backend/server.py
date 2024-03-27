5fafdaf61095c39e4866a3f4d6b424443390d39f
FahmiSar <fahmi.sardar@ucalgary.ca>
2024-03-27 14:44:35 -0600

    PUT and DELETE are done

--- lab10/backend/server.py
+++ lab10/backend/server.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def load_products():
     with open('products.json', 'r') as file:
         return json.load(file)["products"]
-    
+
+# ----------- GET specific or all --------------
 @app.route("/products", methods = ["GET"])
 @app.route("/products/<int:product_id>", methods = ["GET"])
 def get_products(product_id = None):
@@ -24,7 +25,7 @@ def get_products(product_id = None):
         '''
         return jsonify(product) if product else ("", 404)
     
-
+# ----------- POST (create new product) ------------
 @app.route("/products/add", methods=["POST"])
 def add_product():
     new_product = request.json
@@ -41,9 +42,57 @@ def add_product():
         json.dump({"products": products}, file)
     return jsonify(new_product), 201
 
+# ------ Get an image -----------
 @app.route("/product-images/<path:filename>")
 def get_image(filename):
     return send_from_directory("product-images", filename)
+
+# ----------- PUT (update existing product) ----------
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    products = load_products()
+    product_index = None
+    for index, p in enumerate(products):
+        if p["id"] == product_id:
+            product_index = index
+            break
+    if product_index is not None:
+        '''
+        request.json is taking all the additional information we pass to the backend
+        (like brand, price, quantity etc etc through postman) and converting that to json 
+        format
+        '''
+        update_product = request.json 
+        products[product_index].update(update_product)
+        with open('products.json','w') as file:
+            json.dump({"products": products}, file)
+        return jsonify(products[product_index]), 200
+
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    products = load_products()
+    product_index = None
+    for index, product in enumerate(products):
+        if product["id"] == product_id:
+            product_index = index
+            break
+    if product_index is not None:
+        '''
+        products is like a copy of the backend so after the changes we need to dump
+        it in order to have it persist
+        '''
+        products.pop(product_index)
+        with open('products.json', 'w') as file:
+            json.dump({"products": products}, file)
+        """
+        Return statements send information back to client. However with deletions there
+        isn't anything to return back to client since we already updated the json on our backend
+        with the deletion so we just send back nothing along with the status code
+        """
+        return "", 204
+    else:
+        return "", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
